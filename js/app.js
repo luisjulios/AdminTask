e4101025a3976159bd036856789d25a8ecf82048
Luis Julio <antonio.saurith@gmail.com>
2021-09-27 16:41:43 -0300

    Se agregan formularios de proyecto, y tareas. Se agregan class en archivo js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,43 @@
-window.onload = function() {
+window.onload = function () {
+  // crearProyecto();
 }
+let proyectos = [];
+let tareas = [];
+class Project {
+  constructor(name, date, task) {
+    this.name = name;
+    this.date = date;
+    this.task = task;
+  }
+}
+class Task {
+  constructor (name, priority) {
+    this.name = name;
+    this.priority = priority;
+  }
+}
+const crearProyecto = (name, priority, date, task) => {
+  name = prompt("Ingresa el nombre del proyecto:");
+  date = prompt("Ingresa una fecha de entrega:", 'dd/mes/año');
+  let proyecto = new Project(name, priority, date, task);
+  proyectos.push(proyecto);
+}
+const crearTarea = (name, priority) => {
+  task = prompt("Ingresa por lo menos la primera tarea:");
+  priority = parseInt(prompt("Ingresa la prioridad del proyecto del 1 al 5:"));
+  let tarea = new Task(name, priority);
+  tareas.push(tarea);
+}
+
+
+$(function(){
+  $("#fecha").datepicker({
+      minDate: '1d',
+      maxDate: '15d',
+      beforeShowDay: $.datepicker.noWeekends,
+      dateFormat: "dd/mm/yy",
+      dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
+      monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+      firstDay: 1,
+      });
+});
