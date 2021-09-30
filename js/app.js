b7dc0f31ec354778867fabc7b0e6fd729a5e32d2
Luis Julio <antonio.saurith@gmail.com>
2021-09-30 20:01:51 -0300

    Se leen input para agregar tareas, y proyectos al html, y se agrega modo claro/oscuro

--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
-window.onload = function () {
-  crearProyecto();
-  crearTarea();
-}
 class Project {
   constructor(name, date) {
     this.name = name;
@@ -9,18 +5,16 @@ class Project {
   }
 }
 let proyectos = [];
-const crearProyecto = (name, date) => {
-  name = prompt("Ingresa el nombre del proyecto:");
-  date = prompt("Ingresa una fecha de entrega:", 'dd/mes/año');
-  let proyecto = new Project(name, date);
-  proyectos.push(proyecto);
-  const projects = document.getElementById('projects');
-  const li = document.createElement('li');
-  li.innerHTML = `<li class="list-group-item d-flex justify-content-between align-items-center" title="nameProject">${proyecto.name}<span class="badge bg-info rounded-pill m-2" title="Cantidad de tareas del proyecto" id="totalTasks">${tareas.length}</span><span class="badge bg-secondary m-2" title="Fecha de Entrega" id="totalTasks">${proyecto.date}</span></li>`;
-  projects.appendChild(li);
-}
-
-
+// const crearProyecto = (name, date) => {
+//   name = prompt("Ingresa el nombre del proyecto:");
+//   date = prompt("Ingresa una fecha de entrega:", 'dd/mes/año');
+//   let proyecto = new Project(name, date);
+//   proyectos.push(proyecto);
+//   const projects = document.getElementById('projects');
+//   const li = document.createElement('li');
+//   li.innerHTML = `<li class="list-group-item d-flex justify-content-between align-items-center" title="nameProject">${proyecto.name}<span class="badge bg-info rounded-pill m-2" title="Cantidad de tareas del proyecto" id="totalTasks">${tareas.length}</span><span class="badge bg-secondary m-2" title="Fecha de Entrega" id="totalTasks">${proyecto.date}</span></li>`;
+//   projects.appendChild(li);
+// }
 class Task {
   constructor(name, priority) {
     this.name = name;
@@ -28,33 +22,85 @@ class Task {
   }
 }
 let tareas = [];
-const crearTarea = (name, priority) => {
-  let nTareas = parseInt(prompt("Cuantas tareas deseas agregar?"));
-  for (let i = 0; i < nTareas; i++) {
-    name = prompt("Ingresa el nombre de la tarea:");
-    priority = parseInt(prompt("Ingresa una prioridad a la tarea del 1 al 3"));
-    let tarea = new Task(name, priority);
-    tareas.push(tarea);
-    const tasks = document.getElementById('tasks');
-    const li = document.createElement('li');
-    li.innerHTML = `<li class="list-group-item d-flex justify-content-between align-items-center" id="nameTask">${tarea.name}<span class="badge bg-info rounded-pill m-2" title="Prioridad de la tarea" id="priority">${tarea.priority}</span></li>`;
-    tasks.appendChild(li);
-  }
-}
+// const crearTarea = (name, priority) => {
+//   let nTareas = parseInt(prompt("Cuantas tareas deseas agregar?"));
+//   for (let i = 0; i < nTareas; i++) {
+//     name = prompt("Ingresa el nombre de la tarea:");
+//     priority = parseInt(prompt("Ingresa una prioridad a la tarea del 1 al 3"));
+//     let tarea = new Task(name, priority);
+//     tareas.push(tarea);
+//     const tasks = document.getElementById('tasks');
+//     const li = document.createElement('li');
+//     li.innerHTML = `<li class="list-group-item d-flex justify-content-between align-items-center" id="nameTask">${tarea.name}<span class="badge bg-info rounded-pill m-2" title="Prioridad de la tarea" id="priority">${tarea.priority}</span></li>`;
+//     tasks.appendChild(li);
+//   }
+// }
 
-  
+const formProjects = document.getElementById('formProjects')
+const btnP = document.getElementById('btnP')
+
+btnP.addEventListener('click', ()=>{
+  const inputProject = document.getElementById('proyecto');
+  const inputDate = document.getElementById('fecha');
+  // inputProject.addEventListener('input', ()=>{
+  //   let project = inputProject.value;
+  // })
+  // inputDate.addEventListener('input', ()=>{
+  //   let date = inputDate.value;
+  // })
+  const projects = document.getElementById('projects');
+  const li = document.createElement('li');
+  li.innerHTML = `<li class="list-group-item d-flex justify-content-between align-items-center" title="nameProject">${inputProject.value}<span class="badge bg-info rounded-pill m-2" title="Cantidad de tareas del proyecto" id="totalTasks">${tareas.length}</span><span class="badge bg-secondary m-2" title="Fecha de Entrega" id="totalTasks">${inputDate.value.toLocaleDateString()}</span></li>`;
+  projects.appendChild(li);
+  formProjects.reset();
+})
+formProjects.addEventListener('submit', (e)=>{
+  e.preventDefault()
+})
+const btnT = document.getElementById('btnT')
+const formTasks = document.getElementById('formTasks')
+btnT.addEventListener('click', ()=>{
+  const inputTask = document.getElementById('tarea');
+  const selectPriority= document.getElementById('prioridad');
+  // inputTask.addEventListener('input', ()=>{
+  //   let task = inputTask.value;
+  //   console.log(task);
+  // })
+  // selectPriority.addEventListener('input', ()=>{
+  //   let priority = selectPriority.value;
+  //   console.log(priority);
+  // })
+  const tasks = document.getElementById('tasks');
+  const li = document.createElement('li');
+  li.innerHTML = `<li class="list-group-item d-flex justify-content-between align-items-center" id="nameTask">${inputTask.value}<span class="badge bg-info rounded-pill m-2" title="Prioridad de la tarea" id="priority">${selectPriority.value}</span></li>`;
+  tasks.appendChild(li);
+  formTasks.reset();
+})
+formTasks.addEventListener('submit', (e)=>{
+  e.preventDefault();
+})
 
 
 
 
-$(function () {
-  $("#fecha").datepicker({
-    minDate: '1d',
-    maxDate: '15d',
-    beforeShowDay: $.datepicker.noWeekends,
-    dateFormat: "dd/mm/yy",
-    dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
-    monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-    firstDay: 1,
-  });
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const body = document.getElementById('body');
+const toggle = document.getElementById('toggle');
+toggle.addEventListener('click', () => {
+  body.classList.toggle('dark')
+  body.classList.toggle('light')
+})
